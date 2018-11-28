581e9f3579def9fe4914768c016e48f8d2482000
Frank J. Reashore <frank.reashore@bellin.com>
2018-11-28 14:14:12 -0800

    Step 1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,112 @@
 import React, {Component} from 'react';
 import './App.css';
 
-const list = [
-    {
-        title: 'React',
-        url: 'https://reactjs.org',
-        author: 'Jordan Walke',
-        num_comments: '3',
-        points: 4,
-        objectId: 0
-    },
-    {
-        title: 'Redux',
-        url: 'https://redux.js.org',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: '2',
-        points: 4,
-        objectId: 0
-    },
-
-];
+import list from './AppData.js';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            list: list
+        };
+
+        this.onDismiss = this.onDismiss.bind(this);
+    }
+
+    onDismiss(id) {
+        const isNotId = item => item.objectId !== id;
+        const updatedList = this.state.list.filter(isNotId);
+        this.setState({list: updatedList});
+    }
+
     render() {
-        const message = 'This is a short message';
         return (
             <div className="App">
-                <h2>{message}</h2>
+                <form>
+                    <input type="text"/>
+                </form>
+                {this.state.list.map(item =>
+                    <div key={item.objectId}>
+                        <span>
+                            <a href={list.url}>{item.title}</a>
+                        </span>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                        <span>
+                            <button type="button" onClick={() => this.onDismiss(item.objectId)}>Dismiss</button>
+                        </span>
+                    </div>
+                )}
             </div>
         );
     }
 }
+
+//
+// class App extends Component {
+//     render() {
+//         return (
+//             <div className="App">
+//                 {list.map(item =>
+//                     <div key={item.objectId}>
+//                             <span>
+//                                 <a href={list.url}>{item.title}</a>
+//                             </span>
+//                         <span>{item.author}</span>
+//                         <span>{item.num_components}</span>
+//                         <span>{item.points}</span>
+//                     </div>
+//                 )}
+//             </div>
+//         );
+//     }
+// }
+
+// class App extends Component {
+//     render() {
+//         return (
+//             <div className="App">
+//                 {list.map(function (item) {
+//                     return (
+//                         <div key={item.objectId}>
+//                             <span>
+//                                 <a href={list.url}>{item.title}</a>
+//                             </span>
+//                             <span>{item.author}</span>
+//                             <span>{item.num_components}</span>
+//                             <span>{item.points}</span>
+//                         </div>);
+//                 })}
+//             </div>
+//         );
+//     }
+// }
+
+//
+// class App extends Component {
+//     render() {
+//         return (
+//             <div className="App">
+//                 {list.map(function(item) {
+//                     return (<div>{item.title}</div>);
+//                 })}
+//             </div>
+//         );
+//     }
+// }
+
+//
+// class App extends Component {
+//     render() {
+//         const message = 'This is a short message';
+//         return (
+//             <div className="App">
+//                 <h2>{message}</h2>
+//             </div>
+//         );
+//     }
+// }
 
 export default App;
---
+++ src/AppData.js
@@ -0,0 +1,21 @@
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org',
+        author: 'Jordan Walke',
+        num_comments: '3',
+        points: 4,
+        objectId: 0
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: '2',
+        points: 5,
+        objectId: 1
+    },
+];
+
+export default list;
+
