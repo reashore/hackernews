6116a758cbed63ab6e52e6e43d57837bc1247a16
Frank J. Reashore <frank.reashore@bellin.com>
2018-11-28 12:57:03 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org',
+        author: 'Jordan Walke',
+        num_comments: '3',
+        points: 4,
+        objectId: 0
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: '2',
+        points: 4,
+        objectId: 0
+    },
+
+];
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        const message = 'This is a short message';
+        return (
+            <div className="App">
+                <h2>{message}</h2>
+            </div>
+        );
+    }
 }
 
 export default App;
